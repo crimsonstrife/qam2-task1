cfbc49693f38d459f0fd06854a7ecd98188e780d
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-11-28 22:22:02 -0500

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,25 +1,43 @@
 package main;
 
+/**
+ *
+ * @author Patrick Barnhardt
+ *
+ * JAVADOC Location: in the Root of the Project folder - in a folder called JAVADOCS.
+ */
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import main.JDBC;
 
+/**
+ * The type Main controller.
+ */
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Scheduler");
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         JDBC.makeConnection();
         launch(args);
+    }
+
+    /**
+     * Exit Button Event Handler - Close the application.
+     *
+     * @param event triggered by the exit button
+     */
+    public void ExitBtnAction(ActionEvent event) {
+        System.exit(0);
     }
 }
