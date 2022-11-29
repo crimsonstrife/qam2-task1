f89f1316303dd88449e0ec98c954cabdfc62e9ee
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-11-29 08:34:37 -0500

    Update Main.java
    add location detection support

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import org.w3c.dom.Text;
+
 /**
  *
  * @author Patrick Barnhardt
@@ -17,11 +19,33 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import main.JDBC;
+import java.time.ZoneId;
 
 /**
  * The type Main controller.
  */
 public class Main extends Application {
+
+    /**
+     * Configure the Login Fields.
+     */
+    // configure the username field
+    @FXML
+    private TextField login_username;
+    // configure the password field
+    @FXML
+    private PasswordField login_password;
+    // configure the location label
+    @FXML
+    private Text login_location;
+
+    /**
+     * Use Zone ID to set the location label.
+     *
+     */
+    public void initialize() {
+        login_location.setText(ZoneId.systemDefault().toString());
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -30,14 +54,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
     }
-
-    /**
-     * Configure the Login Fields.
-     */
-    // configure the username field
-    @FXML private TextField login_username;
-    // configure the password field
-    @FXML private PasswordField login_password;
 
     public static void main(String[] args) {
         JDBC.makeConnection();
