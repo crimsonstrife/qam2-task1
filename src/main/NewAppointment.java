c596c78e85b6e61c5a8dcb528a0ad82ca418e50c
Patrick Barnhardt <pbarnh1@wgu.edu>
2022-11-29 20:19:34 -0700

    Create NewAppointment.java

---
+++ src/main/NewAppointment.java
@@ -0,0 +1,22 @@
+package main;
+
+import main.JDBC;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import main.Appointments;
+import main.Contacts;
+
+import java.sql.*;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class NewAppointment {
+
+}
