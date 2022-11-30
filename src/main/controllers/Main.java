c9442a88eb8fe04a09827414cbbc8d4c721e1d24
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-11-30 18:56:51 -0500

    Update Main.java

--- src/main/controllers/Main.java
+++ src/main/controllers/Main.java
@@ -561,6 +561,7 @@ public class Main extends Application implements Initializable {
                 Parent root = loader.load();
                 UpdateCustomer controller = loader.getController();
                 controller.updateCustomer((Customers) table_customers.getSelectionModel().getSelectedItem());
+
                 Stage stage = new Stage();
                 stage.setTitle("Update Customer");
                 stage.setScene(new Scene(root, 600, 312));
