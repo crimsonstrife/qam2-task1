bde6b6dd28a6f32c0182325bd1d3f4b07ce03fff
Patrick Barnhardt <pbarnh1@wgu.edu>
2022-11-30 04:43:03 -0700

    Update newCustomer.java

--- src/main/newCustomer.java
+++ src/main/newCustomer.java
@@ -1,5 +1,10 @@
 package main;
-
+/**
+ *
+ * @author Patrick Barnhardt
+ *
+ * JAVADOC Location: in the Root of the Project folder - in a folder called JAVADOCS.
+ */
 public class newCustomer {
 
 }
