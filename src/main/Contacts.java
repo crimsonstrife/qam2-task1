e4a245b930c4a62082493eaddec3b163da6d5cf3
Patrick Barnhardt <contact@patrickbarnhardt.info>
2022-11-30 00:12:53 -0500

    Update Contacts.java

--- src/main/Contacts.java
+++ src/main/Contacts.java
@@ -1,4 +1,74 @@
 package main;
 
 public class Contacts {
+    private int contact_ID;
+    private String contact_Name;
+    private String contact_email;
+
+    /**
+     *
+     *
+     * @param contact_ID
+     * @param contact_Name
+     * @param contact_email
+     */
+    public Contacts(int contact_ID, String contact_Name, String contact_email) {
+        this.contact_ID = contact_ID;
+        this.contact_Name = contact_Name;
+        this.contact_email = contact_email;
+    }
+
+    /**
+     * Get the contact_ID
+     *
+     * @return
+     */
+    public int getContact_ID() {
+        return contact_ID;
+    }
+
+    /**
+     * Set the contact_ID
+     *
+     * @param contact_ID
+     */
+    public void setContact_ID(int contact_ID) {
+        this.contact_ID = contact_ID;
+    }
+
+    /**
+     * Get the contact_Name
+     *
+     * @return
+     */
+    public String getContact_Name() {
+        return contact_Name;
+    }
+
+    /**
+     * Set the contact_Name
+     *
+     * @param contact_Name
+     */
+    public void setContact_Name(String contact_Name) {
+        this.contact_Name = contact_Name;
+    }
+
+    /**
+     * Get the contact_email
+     *
+     * @return
+     */
+    public String getContact_email() {
+        return contact_email;
+    }
+
+    /**
+     * Set the contact_email
+     *
+     * @param contact_email
+     */
+    public void setContact_email(String contact_email) {
+        this.contact_email = contact_email;
+    }
 }
