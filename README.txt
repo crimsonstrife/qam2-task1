efe6789cc5e6a6004289ecb55b4f3a384446ddc1
Patrick Barnhardt <pbarnh1@wgu.edu>
2022-11-30 22:11:11 -0700

    final

---
+++ README.txt
@@ -0,0 +1,12 @@
+Title: Scheduler
+Purpose: This is my project for QAM2 TASK 1 for C195 at WGU
+Author: Patrick Barnhardt
+Contact: pbarnh1@wgu.edu
+Date: 11/30/2022
+Version: 1.0
+IDE: IntelliJ IDEA 2021.1.3 (Community Edition)
+JDK: 17.0.1
+JavaFX: 11.0.2
+MySQL: mysql-connector-java-8.0.25
+How to run: Run Login.java
+Description of additional report: I intended to add a report that displayed the number of appointments created by each user but ran out of time.
---
+++ src/main/Scheduler.java
@@ -0,0 +1,23 @@
+package main;
+
+import main.utilities.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Scheduler extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("resources/views/login.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+}
